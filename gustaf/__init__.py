9d6c8ae686d29d023048da164c04bee3c8d25a51
clemens.fricke <clemens.david.fricke@tuwien.ac.at>
2022-09-07 17:24:34 +0200

    Class unspecific definition of the init_helper class

--- gustaf/__init__.py
+++ gustaf/__init__.py
@@ -13,15 +13,15 @@ try:
     from gustaf import spline
     from gustaf.spline.base import BSpline, NURBS, Bezier, RationalBezier
     from gustaf.spline.ffd import FFD
-except ImportError:
+except ImportError as err:
     # overwrites the all modules which depend on the `splinepy` library
     # with an object which will throw an error
     # as soon as it is used the first time. This means that any non spline
     # functionality works as before, but as soon as these are used a
     # comprehensive exception will be raised which is understandable in
     # contrast to the possible multitude of errors previously possible
-    from gustaf.utils.init_helper import SplineCanNotBeLoadedHelper
-    spline = SplineCanNotBeLoadedHelper()
+    from gustaf.utils.init_helper import LibraryCanNotBeLoadedHelper
+    spline = LibraryCanNotBeLoadedHelper("splinepy")
     BSpline = spline
     NURBS = spline
     Bezier = spline
